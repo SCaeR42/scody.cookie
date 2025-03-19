d9d4b53c4a633b5d7b5938dfac1fca6f68652f3c
SCaeR <i@e2big.ru>
2025-03-19 15:44:50 +0300

    version 1.0.7 замена отображаемого названия

--- install/version.php
+++ install/version.php
@@ -1,6 +1,6 @@
 <?
 $arModuleVersion = [
-	"VERSION" => "1.0.7",
-	"VERSION_DATE" => "2025-02-13 13:20:17"
+	"VERSION" => "1.0.8",
+	"VERSION_DATE" => "2025-02-18 22:35:04"
 ];
 ?>
--- lang/ru/install/index.php
+++ lang/ru/install/index.php
@@ -1,6 +1,6 @@
 <?php
 $MESS['SCODY_COOKIE_INSTALL_TITLE'] = 'SCoDy: Уведомление о cookie';
-$MESS['SCODY_COOKIE_MODULE_NAME'] = 'SCoDy cookie';
+$MESS['SCODY_COOKIE_MODULE_NAME'] = 'SCoDy: Уведомление о cookie (политика куки)';
 $MESS['SCODY_COOKIE_MODULE_DESCRIPTION'] = 'Окно с условиями использования файлов cookie';
 $MESS['SCODY_COOKIE_MODULE_PARTNER_NAME'] = 'SpaceCodinG';
 $MESS['SCODY_COOKIE_MODULE_PARTNER_URI'] = 'https://spacecoding.net';//todo указывать ссылку на страницу модуля
